1276d4867bd36fdcaf1aa104212e1ccc09c5ac50
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-10-14 20:54:50 +0530

    ....

---
+++ src/main/java/com/bootcoding/restaurant/model/RestaurantCollection.java
@@ -0,0 +1,18 @@
+package com.bootcoding.restaurant.model;
+
+import java.util.HashMap;
+
+public class RestaurantCollection {
+
+
+    public void printRest(){
+
+    }
+
+
+    public static void main(String[] args) {
+        RestaurantCollection rc=new RestaurantCollection();
+
+
+    }
+}
